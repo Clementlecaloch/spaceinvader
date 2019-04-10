3280d44308e46d7b1d44e1d985d5618f71037d7a
Clement Le Caloch <clement.le-caloch@etu.unilim.fr>
2019-04-10 15:03:42 +0200

    Debordement espace de jeu

--- src/main/java/fr/unilim/iut/spaceinvaders/SpaceInvaders.java
+++ src/main/java/fr/unilim/iut/spaceinvaders/SpaceInvaders.java
@@ -1,5 +1,6 @@
 package fr.unilim.iut.spaceinvaders;
 
+import fr.unilim.iut.spaceinvaders.utils.DebordementEspaceJeuException;
 import fr.unilim.iut.spaceinvaders.utils.HorsEspaceJeuException;
 
 public class SpaceInvaders {
@@ -15,12 +16,9 @@ public class SpaceInvaders {
 		this.hauteur = hauteur;
 	}
 
-	public String toString() {
-		return recupererEspaceJeuDansChaineASCII();
-	}
-
 	public void deplacerVaisseauVersLaDroite() {
-        if (vaisseau.abscisse()< (longueur-1)) vaisseau.seDeplacerVersLaDroite();
+		if (vaisseau.abscisseLaPlusADroite() < (longueur - 1))
+			vaisseau.seDeplacerVersLaDroite();
 	}
 
 
@@ -54,19 +52,26 @@ public class SpaceInvaders {
 		return vaisseau != null;
 	}
 
-	public void positionnerUnNouveauVaisseau(int x, int y) {
-		if (!estDansEspaceJeu(x, y))
-			throw new HorsEspaceJeuException("Vous êtes en dehors de l'espace jeu");
-
-		vaisseau = new Vaisseau(x, y);
-	}
-
 	private boolean estDansEspaceJeu(int x, int y) {
 		return ((x >= 0) && (x < longueur)) && ((y >= 0) && (y < hauteur));
 	}
 
 	public void deplacerVaisseauVersLaGauche() {
-		if (vaisseau.abscisse()>(0)) 
+		if (vaisseau.abscisseLaPlusAGauche() > (0))
 			vaisseau.seDeplacerVersLaGauche();
+	}
+
+	public void positionnerUnNouveauVaisseau(int longueur, int hauteur, int x, int y) {
+		if (!estDansEspaceJeu(x, y))
+			throw new HorsEspaceJeuException("La position du vaisseau est en dehors de l'espace jeu");
+
+		if ( !estDansEspaceJeu(x+longueur-1,y))
+			throw new DebordementEspaceJeuException("Le vaisseau déborde de l'espace jeu vers la droite à cause de sa longueur");
+		if (!estDansEspaceJeu(x,y-hauteur+1))
+			throw new DebordementEspaceJeuException("Le vaisseau déborde de l'espace jeu vers le bas à cause de sa hauteur");
+
+		vaisseau = new Vaisseau(longueur,hauteur);
+		
+		vaisseau.positionner(x,y);
 	}
 }
